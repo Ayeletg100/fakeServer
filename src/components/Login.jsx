3b29fb6d9b6554b172cb0dd8596e16db87237121
Ayeletg100 <ayeletginsberg@hilma.tech>
2023-11-21 15:47:52 +0200

    components folder

---
+++ src/components/Login.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      <label></label>
+      <input></input>
+      <label></label>
+      <input></input>
+    </div>
+  );
+};
+
+export default Login;
