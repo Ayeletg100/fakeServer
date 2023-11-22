acbdced0f340925bd5f432e7c362a7072ab069a7
Ayeletg100 <ayeletginsberg@hilma.tech>
2023-11-22 09:43:27 +0200

    merge after problem

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,6 @@ import {
   Routes,
 } from "react-router-dom";
 import Login from "./components/Login";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Register from "./components/Register";
 import Register from "./components/Register";
 import { Home } from "./components/Home";
 import "./App.css";
