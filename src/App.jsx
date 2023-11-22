4ae096b400ce2c00f5c57b9af70778c69481645d
YoadRX <yoadrimok@hilma.tech>
2023-11-22 09:24:53 +0200

    adding a nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,40 @@
-import { useState, useNavigate } from "react";
-import "./App.css";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  NavLink,
+  Route,
+  Routes,
+} from "react-router-dom";
 import Login from "./components/Login";
+import Register from "./components/Register";
+import { Home } from "./components/Home";
+import "./App.css";
+import "./components/NavLinkStyle.css";
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <div className="nav-container">
+        <nav>
+          <NavLink className="item" to="/" activeClassName="active" end>
+            Home
+          </NavLink>
+          <NavLink className="item" to="/register" activeClassName="active">
+            Register
+          </NavLink>
+          <NavLink className="item" to="/login" activeClassName="active">
+            Login
+          </NavLink>
+        </nav>
+      </div>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
